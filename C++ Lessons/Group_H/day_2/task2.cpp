7e5e44d4716fad93c1aac0f0e08490e826759098
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-04 18:12:29 +0400

    group H  day_2 task3

---
+++ C++ Lessons/Group_H/day_2/task2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  long long first_power = a*a;
+  long long second_power = b*b;
+  cout<<(long long)first_power + second_power;
+}
