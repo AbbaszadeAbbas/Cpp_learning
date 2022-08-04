09e62c1e85aa8647950bad72414e64d7068a0fa3
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-04 18:43:46 +0400

    group H  day_2 task10

---
+++ C++ Lessons/Group_H/day_2/task10.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a;
+  cin>>a;
+  int first = a/100;
+  int second = a/10%10;
+  int third = a%10;
+  cout<<first+second+third;
+  return 0;
+}
