e176655f24be960d7e67e9dd1b151144e33c9a2a
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-07 16:45:07 +0400

    enterence queations problem a and b

---
+++ C++ Lessons/Enterence questions/ProblemB.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  int ferq = b - a;
+  cout<<b + ferq;
+
+  return 0;
+}
--- C++ Lessons/Group_D/day_1/task3.cpp
+++ C++ Lessons/Group_D/day_1/task3.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
+  // int a;
+  // cin>>a;
+  // int arrays[a][a];
+  // for(int i = 0;i<a;i++){
+  //   for(int j = 0; j < a;j++){
+  //     cin>>arrays[a][a];
+  //   }
+  // }
   int a;
   cin>>a;
-  int arrays[a][a];
-  for(int i = 0; i < a; i++){
-    for(int b = 0; b < a; b++){
-      cin>>arrays[i][b];
-    }
-  }
+  int arrays[a];
 
-
-  return 0;
 }
