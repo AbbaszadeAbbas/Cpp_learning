4f0e950f5abf09a1b8b5000aa033862fdd6b7eca
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-09 11:06:37 +0400

    group h day 3 task 5

---
+++ C++ Lessons/Group_H/day_3/task5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+int main() {
+  int a;
+  cin>>a;
+  if(a<4){
+    cout<<"First"<<endl;
+  }else if (a<7){
+    cout<<"Second"<<endl;
+  }else if (a<10){
+    cout<<"Third"<<endl;
+  }else if (a<13){
+    cout<<"Fourth"<<endl;
+  }
+}
