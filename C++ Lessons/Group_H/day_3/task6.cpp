000f0501ce864796ab9c20a7e77e47105eac40d5
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-09 14:07:39 +0400

    group h day 3 task 6

---
+++ C++ Lessons/Group_H/day_3/task6.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a;
+  cin>>a;
+  if(a==1){
+    cout<<"January";
+  }else if(a==2){
+    cout<<"February";
+  }else if(a==3){
+    cout<<"March";
+  }else if(a==4){
+    cout<<"April";
+  }else if(a==5){
+    cout<<"May";
+  }else if(a==6){
+    cout<<"June";
+  }else if(a==7){
+    cout<<"July";
+  }else if(a==8){
+    cout<<"August";
+  }else if(a==9){
+    cout<<"September";
+  }else if(a==10){
+    cout<<"October";
+  }else if(a==11){
+    cout<<"November";
+  }else if(a==12){
+    cout<<"December";
+  }
+}
