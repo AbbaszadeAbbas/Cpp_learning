2ede20dd28ad91a11141b4d90397c2d73e08653a
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-12 16:59:12 +0400

    grup h day 4 task  9

---
+++ C++ Lessons/Group_H/day_4/task9.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  double a,b,c,d;
+  cin>>a>>b>>c>>d;
+  cout.precision(4);
+  cout<<fixed<<(double)a+b<<endl;
+  cout.precision(4);
+  cout<<fixed<<(double)a+b+c<<endl;
+  cout.precision(4);
+  cout<<fixed<<(double)a+b+c+d;
+}
