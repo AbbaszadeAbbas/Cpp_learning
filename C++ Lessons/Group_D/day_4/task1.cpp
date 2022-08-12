82d77e0fbe78dacaf5b35efd8e15073c54c678a5
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-12 12:38:17 +0400

    group d day 4 task6

--- C++ Lessons/Group_D/day_4/task1.cpp
+++ C++ Lessons/Group_D/day_4/task1.cpp
@@ -1,24 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-  string s;
-  getline(cin, s);
-  for(int i = 0; i<s.length(); i++){
-    if(s[i]=='a'){
-      char b = 'b';
-      swap(s[i], b);
-    }else if(s[i]=='b'){
-      char a = 'a';
-      swap(s[i], a);
+int main(){
+    string input;
+    getline(cin, input);
+    for(int i = 0; i < input.length(); i++){
+        if(input[i] >= 'A' && input[i] <= 'Z'){
+            input[i] = tolower(input[i]);
+        }
+        else if(input[i] >= 'a' && input[i] <= 'z'){
+            input[i] = toupper(input[i]);
+        }
     }
-
-    if(s[i]=='A'){
-      char B = 'B';
-      swap(s[i], B);
-    }else if(s[i]=='B'){
-      char A = 'A';
-      swap(s[i], A);
-    }
-  }
-  cout<<s<<endl;
+    cout<<input<<endl;
 }
