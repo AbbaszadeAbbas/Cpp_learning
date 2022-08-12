c44885e11073c102d4cca22496f92d816e60b1f5
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-12 19:04:19 +0400

    grup d day 4 task  0

---
+++ C++ Lessons/Group_D/day_4/task0.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  string a;
+  int b = 0;
+  getline(cin, a);
+  for(int i = 0; i< a.length(); i++){
+    if(a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U'||a[i]=='Y'){
+      b+=1;
+    }
+    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='y'){
+      b+=1;
+    }
+  }
+  cout<<b<<endl;
+}
