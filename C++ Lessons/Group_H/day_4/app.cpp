77e7a8dc8a7a12c05070228ad88e66507307d338
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-12 17:05:10 +0400

    grup h day 4 task  10

--- C++ Lessons/Group_H/day_4/app.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-  int a,b;
-  cin>>a>>b;
-  cout<<(long long)pow(2, a)+pow(2, b);
-}
---
+++ C++ Lessons/Group_H/day_4/task10.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  long long a,n;
+  cin>>a>>n;
+  long long result = pow(a, n);
+  cout<<result;
+}
