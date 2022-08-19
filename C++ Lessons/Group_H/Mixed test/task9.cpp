319f6d9f0b935fa43c728c4604f7f764b13bf94b
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-19 20:45:28 +0400

    group h mixed tasks task 8

---
+++ C++ Lessons/Group_H/Mixed test/task9.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	int first = a/100;
+	int last = a%10;
+	if(first != last){
+		cout<<max(first, last);
+	}else{
+		cout<<"=";
+	}
+
+}
