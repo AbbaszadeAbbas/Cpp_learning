a185d6f42f1db1425c43954404d4d71f1553cb06
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-23 20:01:22 +0400

    group h day 7 task 3

--- C++ Lessons/Group_H/day_7/task3.cpp
+++ C++ Lessons/Group_H/day_7/task3.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-  double total = 0,say = 0;
+  double total = 0,say = 0, use = 0;
   int n;
   cin>>n;
   double a[n];
   for(int i = 0; i < n;i++){
     cin>>a[i];
     if(a[i]>0){
+      use = 1;
       say+=1;
       total+=a[i];
-    }else{}
-  }
-  for(int i = 0;i<n;i++){
-    if(a[i]<=0){
-      cout<<"Not Found";
-      return 0;
     }
   }
-
-  cout.precision(2);
-  cout<<fixed<<total/say;
+  if(use == 0){
+    cout<<"Not Found";
+    return 0;
+  }
+  if(use = 1){
+    cout.precision(2);
+    cout<<fixed<<total/say;
+  }
 }
