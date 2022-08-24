471223df4c1da2435ab38d13b325ea0785b8f6a7
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-24 12:21:40 +0400

    group h day 7 task 10

---
+++ C++ Lessons/Group_H/day_7/task9.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  long long n;
+  cin>>n;
+  long long a[n];
+  for(long long i = 0; i<n;i++){
+    cin>>a[i];
+  }
+  long long min = a[0],max = a[0],max_index = 0,min_index = 0;
+  for(long long i = 1; i<n;i++){
+    if(a[i]>max){
+      max = a[i];
+      max_index = i;
+    }
+    if(a[i]<min){
+      min = a[i];
+      min_index = i;
+    }
+  }
+  swap(a[max_index], a[min_index]);
+  for(long long i = 0;i<n;i++){
+    cout<<a[i]<<" ";
+  }
+}
