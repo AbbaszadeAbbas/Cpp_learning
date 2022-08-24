2234c87da243c879a0f923a7f75c040d4d4b8bb8
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-24 11:28:22 +0400

    group h day 7 task 7

--- C++ Lessons/Group_H/day_7/task7.cpp
+++ C++ Lessons/Group_H/day_7/task7.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-  ubt 
+  int n ;
+  cin>>n;
+  int a[n];
+  for(int i = 1; i<n+1;i++){
+    cin>>a[i];
+  }
+  a[0] = a[n];
+
+  for(int i = 0; i<n;i++){
+    cout<<a[i]<<" ";
+  }
 }
