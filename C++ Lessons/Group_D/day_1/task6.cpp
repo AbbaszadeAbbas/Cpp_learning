75138a17185af00a797d80ddaabe111aabb58485
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-08-27 09:33:36 +0400

    group d old tasks

--- C++ Lessons/Group_D/day_1/task6.cpp
+++ C++ Lessons/Group_D/day_1/task6.cpp
@@ -3,25 +3,18 @@ using namespace std;
 int main() {
 	int x,y;
 	cin>>x>>y;
-	int a[x][y],b[x][y],total[x][y];
-
+	int a[x][y];
 	for(int i = 0; i < x; i++){
 		for(int j = 0; j < y; j++){
 			cin>>a[i][j];
 		}
 	}
-	for(int i = 0; i < x; i++){
-		for(int j = 0; j < y; j++){
-			cin>>b[i][j];
-		}
-	}
-
-	for(int i = 0; i < x; i++){
-		for(int j = 0; j < y; j++){
-			if(j!=y-1){
-				cout<<a[i][j]+b[i][j]<<" ";
-			}else if(j==y-1){
-				cout<<a[i][j]+b[i][j]<<endl;
+	for(int i = 0; i < y; i++){
+		for(int j = 0; j < x; j++){
+			if(j!=x-1){
+				cout<<a[j][i]<<" ";
+			}else if(j==x-1){
+				cout<<a[j][i]<<endl;
 			}
 		}
 	}
