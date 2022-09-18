aa8bfb2806cd60ddbaa053d16a689b69128445f0
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-09-18 15:57:15 +0400

    group d day 4_5 task 10

---
+++ C++ Lessons/Group_D/September/day4_5/task10.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	char char_helper;
+	string input,helper;
+	int num ;
+	vector<long long>array;
+	while(cin>>input){
+		if(input=="push"){
+            cin>>num;
+            array.push_back(num);
+            cout<<"ok"<<endl;
+		}else if(input=="front"){
+			if(array.size()>0){
+			    cout<<array[0]<<endl;
+			}else{
+			    cout<<"error"<<endl;
+			}
+		}else if(input=="size"){
+			cout<<array.size()<<endl;
+		}else if(input=="pop"){
+            if(array.size()>0){
+                long long ilk = array[0];
+          	    array.erase(array.begin());
+    	        cout<<ilk<<endl;
+            }else{
+                cout<<"error"<<endl;
+            }
+		}else if(input=="clear"){
+			array.clear();
+			cout<<"ok"<<endl;
+		}else if(input=="exit"){
+			cout<<"bye"<<endl;
+			return 0;
+		} 
+	}
+}
