4ff106fa47980545995b83dc4d6b2cee3e2c92eb
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-09-26 22:44:36 +0400

    group d day 7 task2

--- C++ Lessons/Group_D/September/day_7/task2.cpp
+++ C++ Lessons/Group_D/September/day_7/task2.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
     set<long long>result;
-    long long coxluqlarin_sayi,helper,coxlugun_elementlerinin_sayi;
-    cin>>coxluqlarin_sayi>>coxlugun_elementlerinin_sayi;
-    long long coxlug[coxluqlarin_sayi][coxlugun_elementlerinin_sayi];
-    for(long long i = 0;i<coxluqlarin_sayi;i++){
-        for(long long j = 0;j<coxlugun_elementlerinin_sayi;j++){
-            cin>>coxlug[i][j];
-            result.insert(coxlug[i][j]);
+    long long s,s2,helper;
+    cin>>s;
+    for(long long i = 0;i<s;i++){
+        cin>>s2;
+        for(long long j = 0;j<s2;j++){
+            cin>>helper;
+            result.insert(helper);
         }
     }
     cout<<result.size();
