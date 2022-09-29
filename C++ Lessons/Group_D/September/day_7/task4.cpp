a2e41e1f55169fcf46d997bcc3832a3380bfaba6
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-09-29 15:41:36 +0400

    group d day 7 task4

--- C++ Lessons/Group_D/September/day_7/task4.cpp
+++ C++ Lessons/Group_D/September/day_7/task4.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    long long array_size,helper,result,last_element = 0;
-    cin>>array_size;
+    long long s,h;
+    cin>>s;
     set<long long>a;
-    for(int i = 0;i<array_size;i++){
-        cin>>helper;
-        if(helper == last_element){
-            cout<<helper;
-            return 0;
-        }
-        a.insert(helper);
-        last_element = helper;
+    for(long long i = 0;i<s;i++){
+        cin>>h;
+        a.insert(h);
     }
-
+    auto result = a.end();
+    result-- ;
+    result-- ;
+    cout<<*result;
 }
