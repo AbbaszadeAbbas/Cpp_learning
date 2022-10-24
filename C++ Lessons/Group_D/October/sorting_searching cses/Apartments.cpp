e55f60ba1d3e31cf017ab0688561c54dfc752e81
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-10-24 10:44:59 +0400

    sorting searcing cses task 'Apartments'

--- C++ Lessons/Group_D/October/sorting_searching cses/Apartments.cpp
+++ C++ Lessons/Group_D/October/sorting_searching cses/Apartments.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  long long a,b,c,helper,count = 0;
-  cin>>a>>b>>c;
-  vector<long long>v1;
-  vector<long long>v2;
-  for(long long i = 0;i<a;i++){
-    cin>>helper;
-    v1.push_back(helper);
-  }
-  for(long long i = 0;i<b;i++){
-    cin>>helper;
-    v2.push_back(helper);
-  }
-  for(long long i = 0;i<a;i++){
-    for(long long j = 0;j<v2.size();j++){
-      if(v1[i]-c- 1<v2[j] && v2[j] < v1[i]+1){
-        count+=1;
-        v2.erase(v2.begin() + j);
-        break;
-      }else{
-        continue;
-      }
+    long long a,b,c,helper,count = 0,i = 0,j = 0;
+    cin>>a>>b>>c;
+    vector<long long>v1;
+    vector<long long>v2;
+    for(long long i = 0;i<a;i++){
+        cin>>helper;
+        v1.push_back(helper);
     }
-  }
-  cout<<count;
+    for(long long i = 0;i<b;i++){
+        cin>>helper;
+        v2.push_back(helper);
+    }
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    while(i<a && j < b){
+        if(v1[i] + c >= v2[j] && v1[i] - c <= v2[j]){
+            i++;
+            j++;
+            count++;
+        }else if(v1[i]>v2[j]){
+            j++;
+        }else{
+            i++;
+        }
+    }
+    cout<<count<<endl;
 }
