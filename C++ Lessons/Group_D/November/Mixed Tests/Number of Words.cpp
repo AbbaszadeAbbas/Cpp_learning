98b0bf679c4483dda43a4c8a87ddb5701d27e7d8
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-18 19:20:20 +0400

    Mixed test / Number of Word

---
+++ C++ Lessons/Group_D/November/Mixed Tests/Number of Words.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main() {
+    long long count = 1;
+    string s;
+    getline(cin, s);
+    vector<string>v;
+    for(long long i = 0;i<s.size();i++){
+        if(s[i] == ' ' && s[i-1] != ' '){
+            count++;    
+        }
+    }
+    cout<<count;
+}
