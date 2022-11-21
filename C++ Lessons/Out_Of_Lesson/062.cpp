f06a3f27f8836e567a194afbdba224d918e1a678
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-21 20:35:57 +0400

    out of lesson / 062

---
+++ C++ Lessons/Out_Of_Lesson/062.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long a;
+    cin>>a;
+    if(a>0){
+        cout<<1;
+    }else if(a<0){
+        cout<<-1;
+    }else{
+        cout<<0;
+    }
+}
