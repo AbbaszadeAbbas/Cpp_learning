ce23904d7f32f064160dd96babce3013eacc5f21
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-22 11:29:24 +0400

    out of lesson / delete the digit

---
+++ C++ Lessons/Out_Of_Lesson/Delete number 3.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string a;
+    cin>>a;
+    cout<<a[0]<<a[2]<<a[3];
+    return 0;
+}
---
+++ C++ Lessons/Out_Of_Lesson/Delete the digit.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long a;
+    cin>>a;
+    a = a/10;
+    cout<<a;
+    return 0;
+}
