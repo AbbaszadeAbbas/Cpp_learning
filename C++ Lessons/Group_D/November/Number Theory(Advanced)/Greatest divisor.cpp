419cc6c3e6f626dc79718a64785aefc468bbcf37
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-24 00:54:51 +0400

    Number Theory (Advanced) / Greatest Divisor

--- C++ Lessons/Group_D/November/Number Theory(Advanced)/Greatest divisor.cpp
+++ C++ Lessons/Group_D/November/Number Theory(Advanced)/Greatest divisor.cpp
@@ -1,16 +1,15 @@
-#include<iostream>
+#include <bits/stdc++.h>
 #define MAXIMUM 1000000
-using namespace std; 
-int main () {
+using namespace std;
+int main() {
     long long a;
     cin>>a;
-    for(long long i = 1;i<MAXIMUM;i++){
+    long long ans = a;
+    for(long long i = 2;i<MAXIMUM;i++){
         if(a%i == 0){
-            if(i == 1){
-                
-            }
-            cout<<a/i;
-            return 0;
-        } 
+            ans = i;
+            break;
+        }
     }
+    cout<<a/ans;
 }
