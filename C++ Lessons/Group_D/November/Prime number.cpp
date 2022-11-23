1702bc00e6a4b024393757d0cd2a3c21edb42ebb
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-24 00:50:33 +0400

    Number Theory (Advanced) / Prime number 70%

---
+++ C++ Lessons/Group_D/November/Prime number.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define MAXIMUM 1000000
+using namespace std;
+int main() {
+    long long a,ans;
+    cin>>a;
+    for(long long i = 2;i<MAXIMUM;i++){
+        if(a%i == 0){
+            ans = i;
+            break;
+        }
+    }
+    if(ans == a){
+        cout<<"1";
+    }else{
+        cout<<"0";
+    }
+}
