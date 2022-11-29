636fd648c3463fbad6905d2a069e9031baf4c403
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-29 11:21:24 +0400

    Mixed tests / Product of Array Except Self

--- C++ Lessons/Group_D/November/Mixed Tests/Product of Array Except Self.cpp
+++ C++ Lessons/Group_D/November/Mixed Tests/Product of Array Except Self.cpp
@@ -1,20 +1,31 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int main() {
-    long long a,helper,hasil = 1;
-    cin>>a;
-    vector<long long>v;
-    for(long long i = 0;i<a;i++){
-        cin>>helper;
-        if(helper!=1){
-            v.push_back(helper);
+int main(){ 
+    int n,z=1,y,z2 = 1;
+    cin>>n;
+    int check = 0;
+    long long a[n];
+    for(int i=0;i<n;i++){ 
+        cin>>a[i];
+        if(a[i] == 0){
+            check ++;
+        }else if(a[i] != 0){
+            z2*=a[i];
+        }
+        z *= a[i];
+    }
+    for(long long i = 0;i<n;i++){
+        if(check == 0){
+            cout<<z/a[i]<<" ";
+        }else if(check > 0){
+            if(a[i] != 0){
+                cout<<0<<" ";
+            }else if(a[i] == 0 && check == 1){
+                cout<<z2<<" ";
+            }else{
+                cout<<0<<" ";
+            }
         }
     }
-    for(long long i = 0;i<v.size();i++){
-        hasil = 1;
-        for(long long j = 0;j<v.size();j++){
-            hasil *= v[j];
-        }
-        cout<<hasil<<" ";
-    }
+    
 }
