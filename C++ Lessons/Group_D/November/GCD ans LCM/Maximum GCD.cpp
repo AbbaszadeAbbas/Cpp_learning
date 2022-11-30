dbc3f08ea3f23d96c8eac14b92f8ebedde827538
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-11-30 12:21:37 +0400

    GCD and LCM / Maximum GCD

--- C++ Lessons/Group_D/November/GCD ans LCM/Maximum GCD.cpp
+++ C++ Lessons/Group_D/November/GCD ans LCM/Maximum GCD.cpp
@@ -1,15 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    long long size,helper;
+    long long helper,size;
     cin>>size;
     vector<long long>v;
-    for(long long i = 0;i<size;i++){
+    vector<long long>ans;
+    vector<long long>result;
+    for(long long z  = 0;z<size;z++){
         while(cin>>helper){
             v.push_back(helper);
+            if(cin.get() == '\n')break;
         }
-        for(long long j = 0;j<v.size();j++){
-            
+        for(long long i = 0;i<v.size();i++){
+            for(long long j = 0;j<v.size();j++){
+                if(v[i]!=v[j]){
+                    ans.push_back(__gcd(v[i],v[j]));
+                }
+            }
         }
+        sort(ans.begin(), ans.end());
+        result.push_back(ans[ans.size()-1]);
+        v.clear();
+        ans.clear();
     }
+    for(long long i = 0;i<result.size();i++){
+        cout<<result[i]<<endl;
+    }
+    
 }
