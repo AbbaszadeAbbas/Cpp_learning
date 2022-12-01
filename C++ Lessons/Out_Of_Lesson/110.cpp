79ed7d4706835e5fcfcf916808a0d2965315286b
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 23:00:09 +0400

    Out of Lesson / 110

---
+++ C++ Lessons/Out_Of_Lesson/110.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main () { 
+    long long w=0,b,a,d=1,c=0;
+    while(cin>>b){
+        c+=b;
+        if(b==0){
+            cout<<c;
+            break;
+        }
+    }
+}
