39c8a0dc6133036cd1c8e19ec14c1ba1e3e3400d
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 11:07:10 +0400

    Out of Lesson / №Rectangle 3

---
+++ C++ Lessons/Out_Of_Lesson/#Rectangle3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a, b;
+    cin>>a>>b;
+    for(long long i = 0;i<a;i++){
+        for(long long j = 0;j<b;j++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
