c58c6054c7077281c88c63707970212f90500151
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 10:57:18 +0400

    Out of Lesson / №Rectangle 2

---
+++ C++ Lessons/Out_Of_Lesson/#Rectangle 2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string z;
+    long long a;
+    cin>>a;
+    for(long long j = 0;j<a;j++)cout<<'#';
+    cout<<endl;
+    for(long long j = 0;j<a;j++)cout<<'#';
+    cout<<endl;
+    for(long long j = 0;j<a;j++)cout<<'#';
+    cout<<endl;
+    for(long long j = 0;j<a;j++)cout<<'#';
+}
