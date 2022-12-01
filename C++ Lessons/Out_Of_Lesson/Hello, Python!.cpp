085c46be36238d862bed00a9e3d4e5d12cd2759d
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 15:56:40 +0400

    Out of Lesson / Hello, Python!

---
+++ C++ Lessons/Out_Of_Lesson/Hello, Python!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout<<'H';
+    cout<<'e';
+    cout<<'l';
+    cout<<'l';
+    cout<<'o';
+    cout<<',';
+    cout<<' ';
+    cout<<'P';
+    cout<<'y';
+    cout<<'t';
+    cout<<'h';
+    cout<<'o';
+    cout<<'n';
+    cout<<'!';
+}
