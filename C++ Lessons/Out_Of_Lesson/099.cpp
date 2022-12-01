8cc4c42d920ea406d5d8cbdbc447f64f5602eff2
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 16:11:57 +0400

    Out of Lesson / 099

--- C++ Lessons/Out_Of_Lesson/099.cpp
+++ C++ Lessons/Out_Of_Lesson/099.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
-using namespace std;
-int main() {
-    
+using namespace std; 
+int main(){ 
+    long long n,m,l;
+    cin>>n;
+    for (l = 1; l < 1000000000000; l++){ 
+        m = pow(2,l); 
+        if (m > n){ 
+            cout<<m; 
+            break;
+        } 
+    } 
 }
