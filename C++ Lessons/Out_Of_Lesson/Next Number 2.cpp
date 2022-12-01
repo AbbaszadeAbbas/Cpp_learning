f9cc3adb0a9ded803224cf72565f8e306a35edc8
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-01 16:01:40 +0400

    Out of Lesson / Who is extreme

--- C++ Lessons/Out_Of_Lesson/Next Number 2.cpp
+++ C++ Lessons/Out_Of_Lesson/Next Number 2.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std; 
 int main () { 
-    int a,b,c,d,e,f,g,h,k,j,o; 
-    cin >>a; a=a+1; 
+    int a; 
+    cin >>a; 
+    a=a+1; 
     while(a%10!=0){ a++; }
     cout<<a;
 }
