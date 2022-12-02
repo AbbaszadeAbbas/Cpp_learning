8dbde6f187ec89c21296f07fb86d13c7426bc4bc
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-02 16:01:29 +0400

    November / Month contest

--- C++ Lessons/Group_D/November/Month Contest/D) Sugar factory.cpp
+++ C++ Lessons/Group_D/November/Month Contest/D) Sugar factory.cpp
@@ -3,7 +3,12 @@ using namespace std;
 int main() {
     long long a,counter = -1,helper = 0;
     cin>>a;
-    if(a%3 == 0 or a%5 == 0 or a%2 == 0 and a>=5){
+    if(a==3){
+        counter = 1;
+    }else if(a==6){
+        cout<<2;
+    }
+    if(a>7){
         counter = 0;
         while(a>=5){
             a = a - 5;
@@ -13,7 +18,7 @@ int main() {
         if(a==3)  counter++;
         if(a==4)  counter+=2;
         if(a==2)  counter+=2;
-        if(a==1)  counter+=3;
+        if(a==1)  counter++;
     }
     cout<<counter;
 }
