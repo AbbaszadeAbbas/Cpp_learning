7e7323ca9435ef6ada7096345567298afe015bd1
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-09 11:27:47 +0400

    Olymp questions / the essay

---
+++ C++ Lessons/Group_D/December/Olymp questions/the essay.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    unsigned long long a,helper;
+    cin>>a;
+    while(a--){
+        cin>>helper;
+        cout<<helper*helper/2<<endl;
+    }
+}
