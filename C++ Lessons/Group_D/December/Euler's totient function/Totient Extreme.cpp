21ed65a4cca77c0128c7ef5325da5e4f5e080b16
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-19 14:13:53 +0400

    December / Euler's totient function / Totient extreme

--- C++ Lessons/Group_D/December/Euler's totient function/Totient Extreme.cpp
+++ C++ Lessons/Group_D/December/Euler's totient function/Totient Extreme.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int phi(int n){
-    int ans = n;
-    for(int i = 2;i*i<=n;i++){
-        if(n%i==0){
-            while(n%i==0) n/=i;
+int phi(long long a){
+    long long ans = a;
+    long long sq = sqrt(a);
+    for(long long i = 2;i<=sq;i++){
+        if(a%i == 0){
+            while(a%i == 0){a /= i;}
             ans -= ans/i;
-        }   
+        }
     }
-    if(n>1) ans-=ans/n;
+    if(a>1){
+        ans -= ans/a;
+    }
     return ans;
 }
-
 int main() {
-    long long n,helper,h;
-    cin>>n;
-    for(long long z = 0;z<n;z++){
+    long long sum[10001] = {0};
+    for(long long i = 1;i<=10000;i++){
+        sum[i] = sum[i-1] + phi(i);
+    }
+    long long a,helper;
+    cin>>a;
+    while(a--){
         cin>>helper;
-        h = 0;
-        for(long long i = 1;i<=helper;i++){
-            for(long long j = 1;j<=helper;j++){
-                h+=phi(i)*phi(j);
-            }
-        }
-        cout<<h<<endl;
+        cout<<sum[helper]*sum[helper]<<endl;
     }
 }
