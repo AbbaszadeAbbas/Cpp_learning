51894466dff414f021b0a972b556731f7c3130bc
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-22 22:21:56 +0400

    Out of Lesson / An equilateral triangle

---
+++ C++ Lessons/Out_Of_Lesson/An equilateral triangle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long a,b,c;
+    cin>>a>>b>>c;
+    if(a == b and b == c){
+        cout<<a*a;
+    }else{
+        cout<<"No";
+    }
+}
