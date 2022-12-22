b5bbfd5131fe47bf7ad7fc6da52646c60167eb8b
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2022-12-22 22:08:56 +0400

    Out of lesson / Next odd integer

--- C++ Lessons/Out_Of_Lesson/Next even integer.cpp
+++ C++ Lessons/Out_Of_Lesson/Next even integer.cpp
@@ -3,12 +3,9 @@ using namespace std;
 int main(){
     long long a;
     cin>>a;
-    if(a%2 == 0){
-        cout<<a+2;
-        return 0;
-    }
-    if(a%2 == 1){
-        cout<<a+1;
-        return 0;
+    if((a+1) % 2 == 0){
+        cout<<a+1<<endl;
+    }else{
+        cout<<a+2<<endl;
     }
 }
