5dcfce18c06f518a72f6c84286bad7ef2b0f081a
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2023-01-05 13:41:51 +0400

    January(2023) / eolymp / Sum of digits in the text

---
+++ C++ Lessons/Group_D/January(2023)/CSES/Collecting Numbers 2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MAX 9223372036854775807
+using namespace std;
+void result()
+{
+    ll a,b;
+    cin>>a>>b;
+    vector<ll>v;
+    // int num, ans = 1;
+	// cin>>num;
+	// int a, m[num+1];
+	// for(int i = 1; i <= num; i++)
+	// {
+	// 	cin>>a;
+	// 	m[a] = i;
+	// }
+	// for(int i = 1; i <= num; i++)
+	// {
+	// 	if(m[i] < m[i-1]){
+	// 	ans++;
+	// 	}
+	// }
+	// cout<<ans<<endl;
+}
+int main()
+{
+    result();
+    cout<<endl;
+    // system("pause");
+    return 0;
+}
