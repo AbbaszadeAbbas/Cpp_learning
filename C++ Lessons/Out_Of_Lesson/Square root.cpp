93048de60f568693a26a2a84e38c035a0d017852
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2023-01-05 13:54:02 +0400

    January(2023) / eolymp / Square root

--- C++ Lessons/Out_Of_Lesson/Square root.cpp
+++ C++ Lessons/Out_Of_Lesson/Square root.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define ll double
 #define MAX 9223372036854775807
 using namespace std;
 void result()
@@ -9,19 +9,7 @@ void result()
     vector<ll>v;
     set<ll>s;
     cin>>a;
-    for(ll x = 0;x<a;x++)
-    {
-        cin>>helper;
-        v.push_back(helper);
-    }
-    helper = 0;
-    for(ll i =1;i<a;i++)
-    {
-        if(v[i]>v[i-1])
-        {
-            cout<<v[i]<<" ";
-        }
-    }
+    cout<<setprecision(6)<<fixed<<sqrt(a);
 }
 int main()
 {
