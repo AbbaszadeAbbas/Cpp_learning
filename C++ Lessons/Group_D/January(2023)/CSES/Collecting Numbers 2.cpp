f9cac468c55bb833fdaade1944e57d359f073077
AbbaszadeAbbas <mr.abbaszada86@gmail.com>
2023-01-15 13:33:03 +0400

    January / Eolymp / Depth-First Search / Connected components

--- C++ Lessons/Group_D/January(2023)/CSES/Collecting Numbers 2.cpp
+++ C++ Lessons/Group_D/January(2023)/CSES/Collecting Numbers 2.cpp
@@ -1,32 +1,80 @@
+// #include<bits/stdc++.h>
+// #define ll long long
+// #define MAX 9223372036854775806
+// using namespace std;
+// void solve(ll num, ll ans, ll m[])
+// {
+// 	ans = 0;
+// 	for(ll i = 1; i <= num; i++)
+// 	{
+// 		if(m[i] < m[i-1]){
+// 			ans++;
+// 		}
+// 	}
+// 	cout<<ans<<endl;
+// }
+// void result()
+// {
+// 	ll num,b, ans = 1,helper1,helper2;
+// 	cin>>num>>b;
+// 	ll a,m[num+1];
+// 	vector<ll>v;
+// 	for(ll i = 1; i <= num; i++)
+//     {
+//         cin>>a;
+//         // m[a] = i;
+// 		v.push_back(a);
+// 	}
+// 	while(b--)
+// 	{
+// 		cin>>helper1>>helper2;
+// 		swap(v[helper1-1], v[helper2-1]);
+// 		for(ll i = 1;i<=num;i++)
+// 		{
+// 			m[v[i-1]] = i;
+// 		}
+// 		solve(num, ans,m);
+// 	}
+	
+// }
+// // 4 1 2 5 3
+// // 3 1 2 5 4
+// // 3 2 1 5 4
+// int main()
+// {
+// 	result();
+// 	cout<<endl;
+// //     system("pause");
+// 	return 0;
+// }
+
+
 #include<bits/stdc++.h>
 #define ll long long
-#define MAX 9223372036854775807
 using namespace std;
-void result()
-{
-    ll a,b;
-    cin>>a>>b;
-    vector<ll>v;
-    // int num, ans = 1;
-	// cin>>num;
-	// int a, m[num+1];
-	// for(int i = 1; i <= num; i++)
-	// {
-	// 	cin>>a;
-	// 	m[a] = i;
-	// }
-	// for(int i = 1; i <= num; i++)
-	// {
-	// 	if(m[i] < m[i-1]){
-	// 	ans++;
-	// 	}
-	// }
-	// cout<<ans<<endl;
-}
 int main()
 {
-    result();
-    cout<<endl;
-    // system("pause");
-    return 0;
+    ll num,b, ans = 1,helper1,helper2;
+	cin>>num>>b;
+	ll a,m[num+1];
+	vector<ll>v;
+	for(ll i = 1; i <= num; i++)
+    {
+        cin>>a;
+		v.push_back(a);
+	}
+	while(b--)
+	{
+		cin>>helper1>>helper2;ans = 0;
+		swap(v[helper1-1], v[helper2-1]);
+		for(ll i = 1;i<=num;i++)
+		{
+			m[v[i-1]] = i;
+		}
+        for(ll i = 1; i <= num; i++)
+        {
+            if(m[i] < m[i-1])ans++;
+        }
+        cout<<ans<<endl;
+	}
 }
---
+++ C++ Lessons/Group_D/January(2023)/Eolymp/Depth - First Search/Connected Components.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MAX 9223372036854775806
+using namespace std;
+ll color[101];
+ll graph[101][101];
+void solve(ll a,ll number)
+{
+    color[number] = 1;
+    for(ll i = 0;i<a;i++)
+    {
+        if(graph[number][i] == 1 and color[i] == 0)
+        {
+            solve(a, i);
+        }
+    }
+}
+void result()
+{
+    ll a,ans = 0;
+    cin>>a;
+    for(ll i = 0;i<a;i++)
+    {
+        for(ll j = 0;j<a;j++)
+        {
+            cin>>graph[i][j];
+        }
+    }
+    for(ll i = 0;i<a;i++)
+    {
+        if(color[i] == 0)
+        {
+            solve(a, i);
+            ans++;
+        }
+    }
+    cout<<ans;
+}
+int main()
+{
+    result();
+    cout<<endl;
+    system("pause");
+    return 0;
+}
